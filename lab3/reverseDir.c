c7d953fd5f6da7440bec89d3031851c058b28196
Matvei Zhukov <zhukov.matvey.03@mail.ru>
2025-04-23 14:20:13 +0700

    Update reverseDir.c

--- lab3/reverseDir.c
+++ lab3/reverseDir.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -18,16 +17,38 @@ void reverseString(char *str) {
     }
 }
 
-void copyAndReverseFile(const char *srcPath, const char *dstPath) {
+mode_t setPermission(const char *path) {
+    struct stat statBuf;
+    if (stat(path, &statBuf) == -1) {
+        perror("Error permisson reading");
+        return 0;
+    }
+    mode_t originalPermission = statBuf.st_mode & 0777;
+    chmod(path, 0777); 
+    return originalPermission;
+}
+
+void copyAndReverseFile(const char *srcPath, const char *dstPath, mode_t newPermission) {
     int src = open(srcPath, O_RDONLY);
     if (src == -1) {
         perror("Error opening the source file");
         return;
     }
 
+    mode_t oldPermission;
+    int permissionFlag = 0;
+    struct stat statBuf;
+    if (stat(dstPath, &statBuf) != -1) {
+        oldPermission = setPermission(dstPath);
+        permissionFlag = 1; 
+    }
+
     int dst = open(dstPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (dst == -1) {
         perror("Error creating the target file");
+        if (permissionFlag) {
+            chmod(dstPath, oldPermission);
+        }
         close(src);
         return;
     }
@@ -35,6 +56,9 @@ void copyAndReverseFile(const char *srcPath, const char *dstPath) {
     off_t offset = lseek(src, 0, SEEK_END);
     if (offset == -1) {
         perror("File size detection error");
+        if (permissionFlag) {
+            chmod(dstPath, oldPermission);
+        }
         close(src);
         close(dst);
         return;
@@ -55,6 +79,11 @@ void copyAndReverseFile(const char *srcPath, const char *dstPath) {
         bytesRead = read(src, buffer, chunkSize);
         if (bytesRead <= 0) {
             perror("File reading error");
+            if (permissionFlag) {
+                chmod(dstPath, oldPermission);
+            }
+            close(src);
+            close(dst);
             break;
         }
 
@@ -64,14 +93,74 @@ void copyAndReverseFile(const char *srcPath, const char *dstPath) {
             buffer[bytesRead - i - 1] = temp;
         }
 
-        if (write(dst, buffer, bytesRead) != bytesRead) {
-            perror("File writing error");
-            break;
+        write(dst, buffer, bytesRead) != bytesRead;
+    }
+
+    fchmod(dst, newPermission);
+    close(src);
+    close(dst);
+}
+
+void reverseDir(const char *srcDir, const char *dstDir) {
+
+    struct stat dirStat;
+    if (stat(srcDir, &dirStat) == -1) {
+        perror("Error directory permisson reading");
+        return;
+    }
+
+    mode_t oldPermissionsSrc = setPermission(srcDir);
+    mode_t oldPermissionDst;
+    int permissionFlag = 0;
+
+    if (stat(dstDir, &dirStat) == -1) {
+        if (mkdir(dstDir, 0755) != 0) {
+            perror("Folder creation error");
+            chmod(srcDir, oldPermissionsSrc);
+            return;
+        }
+    } else {
+        oldPermissionDst = setPermission(dstDir);
+        permissionFlag = 1;
+    }
+
+    DIR *dir = opendir(srcDir);
+    if (!dir) {
+        perror("Folder opening error");
+        chmod(srcDir, oldPermissionsSrc);
+        if (permissionFlag) {
+            chmod(dstDir, oldPermissionDst);
+        }
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        char srcPath[BUFFER_SIZE];
+        snprintf(srcPath, sizeof(srcPath), "%s/%s", srcDir, entry->d_name);
+
+        char reversedDir[1024];
+        strncpy(reversedDir, entry->d_name, sizeof(reversedDir) - 1);
+        reverseString(reversedDir);
+        char dstPath[BUFFER_SIZE];
+        snprintf(dstPath, sizeof(dstPath), "%s/%s", dstDir, reversedDir);
+
+        if (entry->d_type == DT_REG) {
+            mode_t fileMode = setPermission(srcPath);
+            copyAndReverseFile(srcPath, dstPath, fileMode);
+            chmod(srcPath, fileMode);
+        } else if (entry->d_type == DT_DIR) {
+            reverseDir(srcPath, dstPath);
         }
     }
 
-    close(src);
-    close(dst);
+    closedir(dir);
+    chmod(srcDir, oldPermissionsSrc);
+    chmod(dstDir, oldPermissionsSrc);
 }
 
 int main(int argc, char *argv[]) {
@@ -87,45 +176,13 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    char reversedDir[256];
+    char reversedDir[BUFFER_SIZE];
     strncpy(reversedDir, srcDir, sizeof(reversedDir) - 1);
-    reversedDir[sizeof(reversedDir) - 1] = '\0';
-    reverseString(reversedDir);
+    char *name = strrchr(reversedDir, '/');
+    name = (name != NULL) ? name + 1 : reversedDir;
+    reverseString(name);
 
-    if (mkdir(reversedDir, 0755) != 0) {
-        perror("Folder creation error");
-        return EXIT_FAILURE;
-    }
+    reverseDir(srcDir, reversedDir);
 
-    DIR *dir = opendir(srcDir);
-    if (!dir) {
-        perror("Folder opening error");
-        return EXIT_FAILURE;
-    }
-
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-            continue;
-
-        char srcPath[512];
-        snprintf(srcPath, sizeof(srcPath), "%s/%s", srcDir, entry->d_name);
-
-        struct stat fileStat;
-        if (stat(srcPath, &fileStat) != 0 || !S_ISREG(fileStat.st_mode))
-            continue;
-
-        char reversedFilename[256];
-        strncpy(reversedFilename, entry->d_name, sizeof(reversedFilename) - 1);
-        reversedFilename[sizeof(reversedFilename) - 1] = '\0';
-        reverseString(reversedFilename);
-
-        char dstPath[512];
-        snprintf(dstPath, sizeof(dstPath), "%s/%s", reversedDir, reversedFilename);
-
-        copyAndReverseFile(srcPath, dstPath);
-    }
-
-    closedir(dir);
     return EXIT_SUCCESS;
 }
