9c1ca2a881d99690143f1e86fde1086da567e981
Matvei Zhukov <zhukov.matvey.03@mail.ru>
2025-04-23 14:29:42 +0700

    Update fileManager.c

--- lab3/fileManager.c
+++ lab3/fileManager.c
@@ -8,10 +8,11 @@
 #include <fcntl.h>
  
 void createDirectory(const char *path) {
-    if (mkdir(path, 0755) == 0)
+    if (mkdir(path, 0755) == 0) {
         printf("Folder %s created.\n", path);
-    else
+    } else {
         perror("Folder creation error");
+    }
 }
  
 void listDirectory(const char *path) {
@@ -51,10 +52,11 @@ void removeDirectory(const char *path) {
         }
     }
     closedir(dir);
-    if (rmdir(path) == 0)
+    if (rmdir(path) == 0) {
         printf("Folder %s deleted.\n", path);
-    else
+    } else {
         perror("Folder deletion error");
+    }
 }
  
 void createFile(const char *path) {
@@ -88,17 +90,19 @@ void readFile(const char *path) {
 }
  
 void removeFile(const char *path) {
-    if (unlink(path) == 0)
+    if (unlink(path) == 0) {
         printf("File %s deleted.\n", path);
-    else
+    } else {
         perror("File deletion error");
+    }
 }
  
 void createSymlink(const char *target, const char *linkpath) {
-    if (symlink(target, linkpath) == 0)
+    if (symlink(target, linkpath) == 0) {
         printf("Symbolic link %s -> %s created.\n", linkpath, target);
-    else
+    } else {
         perror("Error creating a symbolic link");
+    }
 }
  
 void readSymlink(const char *path) {
@@ -113,24 +117,27 @@ void readSymlink(const char *path) {
 }
  
 void removeSymlink(const char *path) {
-    if (unlink(path) == 0)
+    if (unlink(path) == 0) {
         printf("Symbolic link %s deleted.\n", path);
-    else
+    } else {
         perror("Error deleting symbolic link");
+    }
 }
  
 void createHardlink(const char *target, const char *linkpath) {
-    if (link(target, linkpath) == 0)
+    if (link(target, linkpath) == 0) {
         printf("Hard link %s -> %s created.\n", linkpath, target);
-    else
+    } else {
         perror("Error creating a hard link");
+    }
 }
  
 void removeHardlink(const char *path) {
-    if (unlink(path) == 0)
+    if (unlink(path) == 0) {
         printf("Hard link %s removed.\n", path);
-    else
+    } else {
         perror("Error deleting a hard link");
+    }
 }
  
 void getFileInfo(const char *path) {
@@ -144,10 +151,11 @@ void getFileInfo(const char *path) {
 }
  
 void changePermissions(const char *path, mode_t mode) {
-    if (chmod(path, mode) == 0)
+    if (chmod(path, mode) == 0) {
         printf("Permissions to %s changed to %o.\n", path, mode);
-    else
+    } else {
         perror("Error changing permissions");
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -159,20 +167,33 @@ int main(int argc, char *argv[]) {
     char *progName = strrchr(argv[0], '/');
     progName = (progName) ? progName + 1 : argv[0];
  
-    if (strcmp(progName, "createDir") == 0) createDirectory(argv[1]);
-    else if (strcmp(progName, "listDir") == 0) listDirectory(argv[1]);
-    else if (strcmp(progName, "removeDir") == 0) removeDirectory(argv[1]);
-    else if (strcmp(progName, "createFile") == 0) createFile(argv[1]);
-    else if (strcmp(progName, "readFile") == 0) readFile(argv[1]);
-    else if (strcmp(progName, "removeFile") == 0) removeFile(argv[1]);
-    else if (strcmp(progName, "createSymlink") == 0 && argc == 3) createSymlink(argv[1], argv[2]);
-    else if (strcmp(progName, "readSymlink") == 0) readSymlink(argv[1]);
-    else if (strcmp(progName, "removeSymlink") == 0) removeSymlink(argv[1]);
-    else if (strcmp(progName, "createHardlink") == 0 && argc == 3) createHardlink(argv[1], argv[2]);
-    else if (strcmp(progName, "removeHardlink") == 0) removeHardlink(argv[1]);
-    else if (strcmp(progName, "fileInfo") == 0) getFileInfo(argv[1]);
-    else if (strcmp(progName, "changePermissions") == 0 && argc == 3) changePermissions(argv[1], strtol(argv[2], NULL, 8));
-    else {
+    if (strcmp(progName, "createDir") == 0) {
+        createDirectory(argv[1]);
+    } else if (strcmp(progName, "listDir") == 0) {
+        listDirectory(argv[1]);
+    } else if (strcmp(progName, "removeDir") == 0) {
+        removeDirectory(argv[1]);
+    } else if (strcmp(progName, "createFile") == 0) {
+        createFile(argv[1]);
+    } else if (strcmp(progName, "readFile") == 0) {
+        readFile(argv[1]);
+    } else if (strcmp(progName, "removeFile") == 0) {
+        removeFile(argv[1]);
+    } else if (strcmp(progName, "createSymlink") == 0 && argc == 3) {
+        createSymlink(argv[1], argv[2]);
+    } else if (strcmp(progName, "readSymlink") == 0) {
+        readSymlink(argv[1]);
+    } else if (strcmp(progName, "removeSymlink") == 0) {
+        removeSymlink(argv[1]);
+    } else if (strcmp(progName, "createHardlink") == 0 && argc == 3) {
+        createHardlink(argv[1], argv[2]);
+    } else if (strcmp(progName, "removeHardlink") == 0) {
+        removeHardlink(argv[1]);
+    } else if (strcmp(progName, "fileInfo") == 0) {
+        getFileInfo(argv[1]);
+    } else if (strcmp(progName, "changePermissions") == 0 && argc == 3) {
+        changePermissions(argv[1], strtol(argv[2], NULL, 8));
+    } else {
         perror("Unknown command or incorrect number of arguments");
         return EXIT_FAILURE;
     }
